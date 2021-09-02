6868a0d39d5e6b86683122056078a911836868f9
esrojas27 <62344298+esrojas27@users.noreply.github.com>
2021-09-02 07:46:10 -0500

    Add files via upload

---
+++ Participaciones/PrecisionIEEE/main.py
@@ -0,0 +1,86 @@
+# Método de Gauss-Jordan
+# Solución a Sistemas de Ecuaciones
+# de la forma A.X=B
+
+import numpy as np
+
+# INGRESO
+A = np.array([[4, 2,  5],
+              [2, 5, 8],
+              [5, 4, 3]])
+
+B = np.array([[50],
+              [60],
+              [56]])
+
+# PROCEDIMIENTO
+casicero = 1e-15  # Considerar como 0
+
+# Evitar truncamiento en operaciones
+A = np.array(A, dtype=float)
+
+# Matriz aumentada
+AB = np.concatenate((A, B), axis=1)
+AB0 = np.copy(AB)
+
+# Pivoteo parcial por filas
+tamano = np.shape(AB)
+n = tamano[0]
+m = tamano[1]
+
+contadorOperaciones = 1
+
+# Para cada fila en AB
+for i in range(0, n - 1, 1):
+    # columna desde diagonal i en adelante
+    columna = abs(AB[i:, i])
+    dondemax = np.argmax(columna)
+
+    # dondemax no está en diagonal
+    if (dondemax != 0):
+        # intercambia filas
+        temporal = np.copy(AB[i, :])
+        AB[i, :] = AB[dondemax + i, :]
+        AB[dondemax + i, :] = temporal
+        contadorOperaciones += 1
+
+AB1 = np.copy(AB)
+
+# eliminacion hacia adelante
+for i in range(0, n - 1, 1):
+    pivote = AB[i, i]
+    adelante = i + 1
+    for k in range(adelante, n, 1):
+        factor = AB[k, i] / pivote
+        AB[k, :] = AB[k, :] - AB[i, :] * factor
+        contadorOperaciones += 1
+AB2 = np.copy(AB)
+
+# elimina hacia atras
+ultfila = n - 1
+ultcolumna = m - 1
+for i in range(ultfila, 0 - 1, -1):
+    pivote = AB[i, i]
+    atras = i - 1
+    for k in range(atras, 0 - 1, -1):
+        factor = AB[k, i] / pivote
+        AB[k, :] = AB[k, :] - AB[i, :] * factor
+        contadorOperaciones += 1
+    # diagonal a unos
+    AB[i, :] = AB[i, :] / AB[i, i]
+X = np.copy(AB[:, ultcolumna])
+X = np.transpose([X])
+
+# SALIDA
+print('Matriz aumentada:')
+print(AB0)
+print('Pivoteo parcial por filas')
+print(AB1)
+print('eliminacion hacia adelante')
+print(AB2)
+print('eliminación hacia atrás')
+print(AB)
+print('solución de X: ')
+print(X)
+print('la cantidad de operaciones fue: ' + str(contadorOperaciones))
+
